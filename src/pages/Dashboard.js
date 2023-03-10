3f249b12414f9fa8da00d968a3eca01949a5c5e0
Asaf Axelrod <AxelrodAsaf@gmail.com>
2023-03-10 18:30:50 +0200

    CSS; Cleanup; Functionality on TableData;

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,11 +6,11 @@ export default function Dashboard() {
 
 
   return (
-    <div className='all' style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-      <h1>Hello there! Welcome to the TLV-Hoops admin area.</h1>
-      <h2>This is where you can see and edit all the information needed.</h2>
-        <div className='navBar' style={{display: "flex", flexDirection: "column"}}>
-          <p onClick={() => navigate(`/tabledata`)}>Live Data</p>
+    <div className='dashboard'>
+        <h1>Hello there! Welcome to the TLV-Hoops admin area.</h1>
+        <h2>This is where you can see and edit all the information needed.</h2>
+        <div className='navBar' style={{ display: "flex", flexDirection: "column" }}>
+          <button onClick={() => navigate(`/tabledata`)}>Live Data</button>
         </div>
 
         <div className='dashboardContent'>
--- src/pages/TableData.js
+++ src/pages/TableData.js
@@ -1,33 +1,64 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import '../../src/App.css'
 
 export default function TableData(props) {
-  const [tableData, setTableData] = useState([]);
+  const [tableData, setTableData] = useState({});
+  const [inputValues, setInputValues] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+  const [errorStyle, setErrorStyle] = useState(false);
   const navigate = useNavigate();
   const tableStyling = { borderCollapse: "collapse", border: "2px solid black", backgroundColor: "rgba(0, 0, 0, 0.15)", width: "100%" }
   const tableHeader = { border: "2px solid black" }
   const tableCell = { border: "2px solid black" }
 
-  const handleDelete = async (topic, itemId) => {
-    // eslint-disable-next-line
-    const response = await axios.delete(`https://tlv-hoops-server.onrender.com/${topic}/${itemId._id}`);
-    // remove the deleted item from the data and update the state
-    const newData = tableData[topic].data.filter(item => item._id !== itemId);
-    setTableData(prevData => ({
-      ...prevData,
-      [topic]: {
-        ...prevData[topic],
-        data: newData
-      }
-    }));
+  function reloadPage() {
+    window.location.reload();
   }
+
+  const handleDelete = async (topic, itemId) => {
+    // Send a request to the server to delete the item
+    console.log(itemId);
+    try {
+      const response = await axios.post(`https://tlv-hoops-server.onrender.com/remove${topic}`,
+        {
+          locationID: itemId.locationID,
+          gameID: itemId.gameID,
+          playerID: itemId.playerID
+        })
+      console.log(response)
+      reloadPage();
+    }
+    catch (error) {
+      console.log(error.message);
+      setErrorStyle(true);
+      setErrorMessage(error.message);
+    }
+  }
+
+  const handleAdd = async (topic) => {
+    console.log(`Trying to add information.`);
+    console.log("Topic:", topic);
+    console.log(inputValues)
+    // Send a request to the server to add the item using the inputData object
+    try {
+      const response = await axios.post(`https://tlv-hoops-server.onrender.com/add${topic}`, inputValues);
+      console.log(response);
+      reloadPage();
+    }
+    catch (error) {
+      console.log(error.message);
+      setErrorStyle(true);
+      setErrorMessage(error.message);
+    }
+  };
 
   useEffect(() => {
     async function getTableData(tableType) {
       const response = await axios.post(`https://tlv-hoops-server.onrender.com/${tableType}list`)
       const tableData = response.data.map(tableItem => {
-        const keysToRemove = ['gameID', 'playerID', '=', 'locationName', 'createdByUser', '_id', 'password', '__v'];
+        const keysToRemove = ['locationName', 'createdByUser', '_id', 'password', '__v'];
         const cleanedTableItem = Object.keys(tableItem).reduce((acc, key) => {
           if (!keysToRemove.includes(key)) {
             acc[key] = tableItem[key];
@@ -45,45 +76,58 @@ export default function TableData(props) {
         }, {});
         return cleanedTableItem;
       })
+      const headers = [...Object.keys(tableData[0]), ''];
+      const hideHeaders = ["gameID", ".", "playerID"];
+      const displayHeaders = headers.filter(header => !hideHeaders.includes(header));
 
       setTableData(prevData => ({
         ...prevData,
         [tableType]: {
           data: tableData,
-          headers: [...Object.keys(tableData[0]), '']
+          headers: displayHeaders
         }
       }));
     }
 
     async function getTables() {
-      const tableTopics = ["game", "player", "location"];
-      for (const tableTopic of tableTopics) {
+      ["game", "player", "location"].forEach(async (tableTopic) => {
         await getTableData(tableTopic);
-      }
+      });
     }
 
     getTables();
   }, [])
 
   return (
-    <div className='all'>
-      <div style={{ display: "flex", width: "95%", height: "100%", alignItems: "center", justifyContent: "center" }}>
-        <div style={{ width: "10%", height: "100%", textAlign: "left", display: "flex", flexDirection: "column" }}>
-          <h1 onClick={() => navigate(`/dashboard`)}>Main</h1>
-          <h1 onClick={() => navigate(`/addData`)}>Add</h1>
+    <div style={errorStyle ? {
+      border: "red 5px solid",
+      borderRadius: "5px",
+      backgroundColor: "rgba(255,0,0, 0.15)"
+    } : null}>
+      <div className='tableData'>
+        <div className='navbar'>
+          <h1 onClick={() => navigate(`/dashboard`)}>Back</h1>
         </div>
-        <br />
-        <div style={{ width: "90%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
-          {["game", "player", "location"].map(topic => (
-            <div key={topic}>
+        <h5>Please make sure to check and uncheck boxes as needed.</h5>
+        <div className='pageContent'>
+          <div style={errorStyle ? {
+            color: "red",
+            fontWeight: "bold",
+            fontSize: "200%",
+            textDecoration: "underline",
+          } : { display: "none" }}>
+            {errorMessage}
+          </div>
+          {["game", "location", "player"].map(topic => (
+            <div className='topicTable' key={topic}>
               <h1>{`All ${topic}s`}</h1>
               <table style={tableStyling}>
                 <thead>
                   <tr>
                     {tableData[topic]?.headers.map((header, index) => (
-                      <th key={index} style={tableHeader}>{header}</th>
+                      <th key={index} style={{ ...tableHeader }}>{header}</th>
                     ))}
-                    <th>Delete</th>
+                    <th>+/x</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -103,12 +147,68 @@ export default function TableData(props) {
                       </td>
                     </tr>
                   ))}
+                  {topic !== 'player' && (
+                    <tr className='addInformation'>
+                      {tableData[topic]?.headers.map((header, index) => (
+                        <td key={index} style={tableCell}>
+                          {header === 'approved' ||
+                            header === 'tlvpremium' ||
+                            header === 'indoor' ||
+                            header === 'lockerRoom' ||
+                            header === 'bathroom' ||
+                            header === 'showers' ||
+                            header === 'vendingMachine' ||
+                            header === 'admin' ? (
+                            <input
+                              type="checkbox"
+                              onChange={(e) =>
+                                setInputValues({
+                                  ...inputValues,
+                                  [header]: e.target.checked,
+                                })
+                              }
+                            />
+                          ) : (header === 'participants') ? (
+                            null
+                          ) : (
+                            header === 'date' ||
+                            header === 'startTime' ||
+                            header === 'endTime' ||
+                            header === 'maximumPlayers' ||
+                            header === 'ageMin' ||
+                            header === 'ageMax' ||
+                            header === 'price' ||
+                            header === 'courtNumber' ||
+                            header === 'benchSpace'
+                          ) ? (
+                            <input type="number" placeholder={header} onChange={(e) => setInputValues({ ...inputValues, [header]: e.target.value })} />
+                          ) :
+                            (
+                              header.trim() !== '' && (
+                                <input
+                                  placeholder={header}
+                                  onChange={(e) =>
+                                    setInputValues({
+                                      ...inputValues,
+                                      [header]: e.target.value,
+                                    })
+                                  }
+                                />
+                              )
+                            )}
+                        </td>
+                      ))}
+                      <td style={tableCell}>
+                        <button onClick={() => handleAdd(topic)}>+</button>
+                      </td>
+                    </tr>
+                  )}
                 </tbody>
               </table>
             </div>
           ))}
         </div>
       </div>
-    </div>
-  )
+    </div >
+  );
 }
